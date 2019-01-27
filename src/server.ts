a665ed54ba5c49909d66784a82e2a38697f65cac
Albert Martinez <albertstartup@gmail.com>
2019-01-27 11:39:01 -0800

    Add mongo url.

--- src/server.ts
+++ src/server.ts
@@ -9,19 +9,43 @@ import path from 'path';
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-const url = 'mongodb://localhost:27017';
+const prod_mongo_url = process.env.MONGODB_URI;
+
+const mongo_url = prod_mongo_url || 'mongodb://localhost:27017';
 
 const dbName = 'wish1';
 
 
 // @ts-ignore
-MongoClient.connect(url, function(err, client) {
+const insertDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('documents');
+    // Insert some documents
+
+    // @ts-ignore
+    const cb = function(err, result) {
+        assert.equal(err, null);
+        assert.equal(3, result.result.n);
+        assert.equal(3, result.ops.length);
+        console.log("Inserted 3 documents into the collection");
+        callback(result);
+    };
+
+    collection.insertMany([
+        {a : 1}, {a : 2}, {a : 3}
+    ], cb);
+};
+
+// @ts-ignore
+MongoClient.connect(mongo_url, function(err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to mongo");
 
     const db = client.db(dbName);
 
-    client.close();
+    insertDocuments(db, function() {
+        client.close();
+    });
 });
 
 const port = process.env.PORT || 4242;
